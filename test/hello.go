23afd3c55f0a03fcebc3cece4341563a51748737
tao.yang <tao.yang@daocloud.io>
2022-03-23 15:45:51 +0800

    test

---
+++ test/hello.go
@@ -0,0 +1,11 @@
+//go:build tools
+// +build tools
+
+// Copyright 2022 Authors of spidernet-io
+// SPDX-License-Identifier: Apache-2.0
+
+package test
+
+import (
+	_ "github.com/onsi/ginkgo/v2/ginkgo"
+)

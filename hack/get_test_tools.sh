b34c097b143b9bd1cb3cecd9fad6d487e002dfc3
tao.yang <tao.yang@daocloud.io>
2022-04-13 18:36:08 +0800

    e2e-test

--- hack/get_test_tools.sh
+++ hack/get_test_tools.sh
@@ -4,6 +4,27 @@
 # SPDX-License-Identifier: Apache-2.0
 
 set -o errexit
+# ensure this file is sourced to add required components to PATH
+
+here="$(dirname "$(readlink --canonicalize "${BASH_SOURCE[0]}")")"
+root="$(readlink --canonicalize "$here/..")"
+VERSION="v0.11.0"
+KIND_BINARY_URL="https://github.com/kubernetes-sigs/kind/releases/download/${VERSION}/kind-$(uname)-amd64"
+K8_STABLE_RELEASE_URL="https://storage.googleapis.com/kubernetes-release/release/stable.txt"
+
+if [ ! -d "${root}/bin" ]; then
+    mkdir "${root}/bin"
+fi
+
+echo "retrieving kind"
+curl --max-time 10 --retry 10 --retry-delay 5 --retry-max-time 60 -Lo "${root}/bin/kind" "${KIND_BINARY_URL}"
+chmod +x "${root}/bin/kind"
+
+echo "retrieving kubectl"
+curl -Lo "${root}/bin/kubectl" "https://storage.googleapis.com/kubernetes-release/release/$(curl -s ${K8_STABLE_RELEASE_URL})/bin/linux/amd64/kubectl"
+chmod +x "${root}/bin/kubectl"
+
+export PATH="$PATH:$root/bin"
 
 #go version &> /dev/null
 #if [ $? -eq 127 ]; then
@@ -22,31 +43,31 @@ set -o errexit
 # fi
 
 # kind
-which kind
-if [ $? -ne 0 ]; then
-  curl --max-time 10 --retry 10 --retry-delay 5 --retry-max-time 60 -Lo /usr/local/bin/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.11.1/kind-linux-amd64	
-  chmod +x /usr/local/bin/kind
-fi 
+# which kind
+# if [ $? -ne 0 ]; then
+#   curl --max-time 10 --retry 10 --retry-delay 5 --retry-max-time 60 -Lo /usr/local/bin/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.11.1/kind-linux-amd64	
+#   chmod +x /usr/local/bin/kind
+# fi 
 
-# kubectl
-which kubectl 
-if [ $? -ne 0 ]; then
-  curl --max-time 10 --retry 10 --retry-delay 5 --retry-max-time 60 -Lo /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-  chmod +x /usr/local/bin/kubectl
-fi
+# # kubectl
+# which kubectl 
+# if [ $? -ne 0 ]; then
+#   curl --max-time 10 --retry 10 --retry-delay 5 --retry-max-time 60 -Lo /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+#   chmod +x /usr/local/bin/kubectl
+# fi
 
 # jq
-which jq 
-if [ $? -ne 0 ]; then
-  curl --max-time 10 --retry 10 --retry-delay 5 --retry-max-time 60 -Lo /usr/local/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
-  chmod +x /usr/local/bin/jq
-fi
+# which jq 
+# if [ $? -ne 0 ]; then
+#   curl --max-time 10 --retry 10 --retry-delay 5 --retry-max-time 60 -Lo /usr/local/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+#   chmod +x /usr/local/bin/jq
+# fi
 
-# helm
-which helm 
-if [ $? -ne 0 ]; then
-  curl --max-time 10 --retry 10 --retry-delay 5 --retry-max-time 60 -Lo helm.tar.gz "https://get.helm.sh/helm-v3.8.1-linux-amd64.tar.gz"
-  tar -xzvf helm.tar.gz && mv linux-amd64/helm /usr/local/bin
-  chmod +x /usr/local/bin/helm
-  rm -f helm.tar.gz
-fi
+# # helm
+# which helm 
+# if [ $? -ne 0 ]; then
+#   curl --max-time 10 --retry 10 --retry-delay 5 --retry-max-time 60 -Lo helm.tar.gz "https://get.helm.sh/helm-v3.8.1-linux-amd64.tar.gz"
+#   tar -xzvf helm.tar.gz && mv linux-amd64/helm /usr/local/bin
+#   chmod +x /usr/local/bin/helm
+#   rm -f helm.tar.gz
+# fi

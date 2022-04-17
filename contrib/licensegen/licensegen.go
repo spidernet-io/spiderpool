e04693f3409ebe00dc7807d57a8f3b1acd208234
Icarus9913 <icaruswu66@qq.com>
2022-04-17 20:44:31 +0800

    license header add for golang files
    Signed-off-by: Icarus9913 <icaruswu66@qq.com>

--- contrib/licensegen/licensegen.go
+++ contrib/licensegen/licensegen.go
@@ -4,14 +4,36 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"io"
+	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
+var (
+	IsCheckLicense bool
+
+	targetPath      = "./api"
+	licenseFilePath = "./hack/spdx-copyright-header.txt"
+)
+
 func main() {
+	if IsCheckLicense {
+		licenseCheck()
+	}
+
+	err := LicenseAdd(targetPath, licenseFilePath, false)
+	if nil != err {
+		log.Fatal(err)
+	}
+}
+
+func licenseCheck() {
 	err := filepath.Walk("./vendor", func(path string, _ os.FileInfo, _ error) error {
 		base := filepath.Base(path)
 		ext := filepath.Ext(base)
@@ -31,4 +53,66 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// LicenseAdd will help you find and add license header for *.go files.
+// The first param is the dir that you wanna check, the second param is the license path,
+// the third param defines whether you want to add license header for them.
+func LicenseAdd(targetPath, licenseFilePath string, addCopy bool) error {
+	licenseFile, err := os.Open(licenseFilePath)
+	if nil != err {
+		return err
+	}
+	licenseReader := bufio.NewReader(licenseFile)
+	var li []byte
+	for {
+		line, err := licenseReader.ReadString('\n')
+		if nil != err || err == io.EOF {
+			if line == "" {
+				break
+			}
+		}
+		li = append(li, []byte("// "+line)...)
+	}
+
+	var noLicenseFileCount int
+	err = filepath.Walk(targetPath, func(path string, _ fs.FileInfo, _ error) error {
+		base := filepath.Base(path)
+		ext := filepath.Ext(base)
+		if ext == ".go" {
+			body, err := os.ReadFile(path)
+			if nil != err {
+				return err
+			}
+			if !bytes.Contains(body, li) {
+				noLicenseFileCount++
+				fmt.Println("Found no license header file:", path)
+				if addCopy {
+					f, err := os.OpenFile(path, os.O_WRONLY, 0644)
+					if nil != err {
+						return err
+					}
+					seek, err := f.Seek(0, 0)
+					if nil != err {
+						return err
+					}
+					res := append(li, '\n', '\n')
+					body = append(res, body...)
+					_, err = f.WriteAt(body, seek)
+					if nil != err {
+						return err
+					}
+				}
+			}
+		}
+		return nil
+	})
+	if nil != err {
+		return err
+	}
+
+	if noLicenseFileCount == 0 {
+		fmt.Printf("All dir %s files have license header.", targetPath)
+	}
+	return nil
 }

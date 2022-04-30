c893a3482412144b0dd7b72b7a27a1fe76f5c025
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2022-05-01 00:43:59 +0800

    typo
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- test/scripts/install-macvlan.sh
+++ test/scripts/install-macvlan.sh
@@ -2,10 +2,16 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright Authors of Spider-net
 
+E2E_CLUSTER_NAME="$1"
+CNI_CONF_PATH="$2"
+
+[ -z "$E2E_CLUSTER_NAME" ] && echo "error, miss E2E_CLUSTER_NAME " && exit 1
+[ -z "$CNI_CONF_PATH" ] && echo "error, miss CNI_CONF_PATH " && exit 1
+[ ! -f "$CNI_CONF_PATH" ] && echo "error, could not find file $CNI_CONF_PATH " && exit 1
+
 # Copy 10-macvlan.conflist to kind-node
-NODES=($(docker ps | grep -w $1 | awk '{print $1}'))
-for node in ${NODES[@]}
-do
-  echo "docker cp $2 $node:/etc/cni/net.d"
-  docker cp $2 $node:/etc/cni/net.d
+NODES=$(docker ps | grep -E "kindest/node.* ${E2E_CLUSTER_NAME}-" | awk '{print $1}')
+for node in ${NODES} ; do
+  echo "docker cp ${CNI_CONF_PATH} $node:/etc/cni/net.d"
+  docker cp ${CNI_CONF_PATH} $node:/etc/cni/net.d
 done

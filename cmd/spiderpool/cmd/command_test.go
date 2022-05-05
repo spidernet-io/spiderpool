a380c91c94d7e6bfc66854f4c3a23a2da4c243f8
Icarus9913 <icaruswu66@qq.com>
2022-05-05 14:31:02 +0800

    1.fix unix socket path already listen bug 2.solve conflicts to restore unix server 3.fix cobra local flags bug
    Signed-off-by: Icarus9913 <icaruswu66@qq.com>

--- cmd/spiderpool/cmd/command_test.go
+++ cmd/spiderpool/cmd/command_test.go
@@ -5,6 +5,7 @@ package cmd_test
 
 import (
 	"fmt"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -17,12 +18,14 @@ import (
 const ifname string = "eth0"
 const nspath string = "/some/where"
 const containerID string = "dummy"
+const CNITimeoutSec = 220
 
 var cniVersion string
 var args *skel.CmdArgs
 var netConf = func(cniVersion string) string {
 	return "{\"cniVersion\": \"" + cniVersion + "\",\"name\": \"spiderpool-ipam\",\"ipam\":{\"type\": \"spiderpool\"}}"
 }
+var addChan, delChan chan struct{}
 
 var _ = Describe("spiderpool plugin", Label("unitest", "ipam_plugin_test"), func() {
 	BeforeEach(func() {
@@ -33,6 +36,9 @@ var _ = Describe("spiderpool plugin", Label("unitest", "ipam_plugin_test"), func
 		}
 
 		cniVersion = "0.3.1"
+
+		addChan = make(chan struct{})
+		delChan = make(chan struct{})
 	})
 
 	It(fmt.Sprintf("[%s] allocates addresses with ADD/DEL", cniVersion), func() {
@@ -40,16 +46,28 @@ var _ = Describe("spiderpool plugin", Label("unitest", "ipam_plugin_test"), func
 		args.StdinData = []byte(conf)
 
 		// Allocate the IP
-		_, _, err := testutils.CmdAddWithArgs(args, func() error {
-			return cmd.CmdAdd(args)
-		})
-		Expect(err).NotTo(HaveOccurred())
+		go func() {
+			defer GinkgoRecover()
+
+			_, _, err := testutils.CmdAddWithArgs(args, func() error {
+				return cmd.CmdAdd(args)
+			})
+			Expect(err).NotTo(HaveOccurred())
+			close(addChan)
+		}()
+		Eventually(addChan).WithTimeout(CNITimeoutSec * time.Second).Should(BeClosed())
 
 		// Release the IP
-		err = testutils.CmdDelWithArgs(args, func() error {
-			return cmd.CmdDel(args)
-		})
-		Expect(err).NotTo(HaveOccurred())
+		go func() {
+			defer GinkgoRecover()
+
+			err = testutils.CmdDelWithArgs(args, func() error {
+				return cmd.CmdDel(args)
+			})
+			Expect(err).NotTo(HaveOccurred())
+			close(delChan)
+		}()
+		Eventually(delChan).WithTimeout(CNITimeoutSec * time.Second).Should(BeClosed())
 	})
 
 	It(fmt.Sprintf("[%s] is returning an error on conf broken with ADD/DEL", cniVersion), func() {
@@ -57,15 +75,27 @@ var _ = Describe("spiderpool plugin", Label("unitest", "ipam_plugin_test"), func
 		args.StdinData = []byte(conf)
 
 		// Allocate the IP
-		_, _, err := testutils.CmdAddWithArgs(args, func() error {
-			return cmd.CmdAdd(args)
-		})
-		Expect(err).To(HaveOccurred())
+		go func() {
+			defer GinkgoRecover()
+
+			_, _, err := testutils.CmdAddWithArgs(args, func() error {
+				return cmd.CmdAdd(args)
+			})
+			Expect(err).To(HaveOccurred())
+			close(addChan)
+		}()
+		Eventually(addChan).WithTimeout(CNITimeoutSec * time.Second).Should(BeClosed())
 
 		// Release the IP
-		err = testutils.CmdDelWithArgs(args, func() error {
-			return cmd.CmdDel(args)
-		})
-		Expect(err).To(HaveOccurred())
+		go func() {
+			defer GinkgoRecover()
+
+			err = testutils.CmdDelWithArgs(args, func() error {
+				return cmd.CmdDel(args)
+			})
+			Expect(err).To(HaveOccurred())
+			close(delChan)
+		}()
+		Eventually(delChan).WithTimeout(CNITimeoutSec * time.Second).Should(BeClosed())
 	})
 })

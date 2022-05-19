0c7c7bd64e1149a0f4bca9af169ff5e3ae2db77a
root <wei.yang@daocloud.io>
2022-05-19 22:57:14 +0800

    add daemonset testcase E00004

---
+++ test/e2e/ippool/daemonset_pod_test.go
@@ -0,0 +1,89 @@
+// Copyright 2022 Authors of spidernet-io
+// SPDX-License-Identifier: Apache-2.0
+package ippool_test
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/spidernet-io/e2eframework/tools"
+	"github.com/spidernet-io/spiderpool/test/e2e/common"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var _ = Describe("test daemonset", Label("ippool_daemonset"), func() {
+	var dsName, nsName string
+	var err error
+	BeforeEach(func() {
+		nsName = "ns" + tools.RandomName()
+		GinkgoWriter.Printf("create namespace %v \n", nsName)
+		err := frame.CreateNamespace(nsName)
+		Expect(err).NotTo(HaveOccurred(), "failed to create namespace %v", nsName)
+		dsName = "dstwo" + tools.RandomName()
+
+		// DeferCleanup(func() {
+		// 	GinkgoWriter.Printf("delete namespace %v \n", nsName)
+		// 	err = frame.DeleteNamespace(nsName)
+		// 	Expect(err).NotTo(HaveOccurred(), "failed to delete namespace %v", nsName)
+		// })
+	})
+
+	Context("test by means of daemonset create pods，assign ipv4、ipv6", func() {
+		It(" pods in a daemonset allocate/release ipv4 and ipv6 addresses", Label("smoke"), Label("E00004"), func() {
+			// create daemonset
+			GinkgoWriter.Printf("try to create daemonset %v/%v \n", nsName, dsName)
+			ds := common.GenerateExampleDaemonSetYaml(dsName, nsName)
+			GinkgoWriter.Printf("GenerateExampleDaemonSetYaml: %v \n", ds)
+			err = frame.CreateDaemonSet(ds)
+			Expect(err).NotTo(HaveOccurred(), "failed to create daemonset")
+
+			// Sometimes the daemonset being created，so wait for daemonset create success ---> hardcode，to do
+			// As the replicas increase，can change the waiting time。
+			// but the same case，last time it worked, this time it didn't，please check performance
+			ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+			defer cancel()
+			ds, err := frame.WaitDaemonSetReady(dsName, nsName, ctx)
+			Expect(err).NotTo(HaveOccurred(), "time out to wait  all Replicas ready")
+			Expect(ds).NotTo(BeNil())
+			//GinkgoWriter.Printf("CurrentNumberScheduled: %v \n", ds.Status.CurrentNumberScheduled)
+			//GinkgoWriter.Printf("DesiredNumberScheduled: %v \n", ds.Status.DesiredNumberScheduled)
+			// get all daemonset replicas name and check ip
+			opts := []client.ListOption{
+				client.InNamespace(nsName),
+				client.MatchingLabels(ds.Spec.Selector.MatchLabels),
+			}
+
+			podinfolist, err := frame.GetPodList(opts...)
+			Expect(err).NotTo(HaveOccurred(), "failed to list pod")
+			//GinkgoWriter.Printf("podinfolist: %v", *podinfolist)
+			//Expect(len(podinfolist.Items)).NotTo(HaveValue(Equal(0)))
+			Expect(len(podinfolist.Items)).NotTo(HaveValue(Equal(ds.Status.CurrentNumberScheduled)))
+
+			// check all pod assign ipv4 and ipv6 addresses success
+			for i := 0; i < len(podinfolist.Items); i++ {
+				// pod, err := frame.WaitPodStarted(podinfolist.Items[i].Name, nsName, ctx)
+				ds, err := frame.GetPod(podinfolist.Items[i].Name, nsName)
+				Expect(err).NotTo(HaveOccurred(), "time out to wait pod ready")
+				Expect(ds).NotTo(BeNil())
+				Expect(ds.Status.PodIPs).NotTo(BeEmpty(), "pod failed to assign ip")
+				GinkgoWriter.Printf("pod %v/%v ip: %+v \n", nsName, dsName, ds.Status.PodIPs)
+
+				if frame.Info.IpV4Enabled == true {
+					Expect(tools.CheckPodIpv4IPReady(ds)).To(BeTrue(), "pod failed to get ipv4 ip")
+					By("succeeded to check pod ipv4 ip ")
+				}
+				if frame.Info.IpV6Enabled == true {
+					Expect(tools.CheckPodIpv6IPReady(ds)).To(BeTrue(), "pod failed to get ipv6 ip")
+					By("succeeded to check pod ipv6 ip \n")
+				}
+				//}
+				// delete daemonset
+				//GinkgoWriter.Printf("delete daemonset: %v \n", dsName)
+				//err = frame.DeleteDaemonSet(dsName, nsName)
+				//Expect(err).NotTo(HaveOccurred(), "failed to delete daemonset: %v", dsName)
+			}
+		})
+	})
+})
---
+++ vendor/github.com/spidernet-io/e2eframework/framework/daemonset.go
@@ -0,0 +1,141 @@
+// Copyright 2022 Authors of spidernet-io
+// SPDX-License-Identifier: Apache-2.0
+package framework
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	"github.com/spidernet-io/e2eframework/tools"
+	appsv1 "k8s.io/api/apps/v1"
+	api_errors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/watch"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func (f *Framework) CreateDaemonSet(ds *appsv1.DaemonSet, opts ...client.CreateOption) error {
+	// try to wait for finish last deleting
+
+	fake := &appsv1.DaemonSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: ds.ObjectMeta.Namespace,
+			Name:      ds.ObjectMeta.Name,
+		},
+	}
+	key := client.ObjectKeyFromObject(fake)
+	existing := &appsv1.DaemonSet{}
+	e := f.GetResource(key, existing)
+	if e == nil && existing.ObjectMeta.DeletionTimestamp == nil {
+		return fmt.Errorf("failed to create , a same daemonset %v/%v exists", ds.ObjectMeta.Namespace, ds.ObjectMeta.Name)
+	} else {
+		t := func() bool {
+			existing := &appsv1.DaemonSet{}
+			e := f.GetResource(key, existing)
+			b := api_errors.IsNotFound(e)
+			if !b {
+				f.t.Logf("waiting for a same daemonset %v/%v to finish deleting \n", ds.ObjectMeta.Namespace, ds.ObjectMeta.Name)
+				return false
+			}
+			return true
+		}
+		if !tools.Eventually(t, f.Config.ResourceDeleteTimeout, time.Second) {
+			return fmt.Errorf("time out to wait a deleting daemonset")
+		}
+	}
+	return f.CreateResource(ds, opts...)
+}
+
+func (f *Framework) DeleteDaemonSet(name, nsName string, opts ...client.DeleteOption) error {
+	pod := &appsv1.DaemonSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: nsName,
+			Name:      name,
+		},
+	}
+	return f.DeleteResource(pod, opts...)
+}
+
+func (f *Framework) GetDaemonSet(name, namespace string) (*appsv1.DaemonSet, error) {
+	ds := &appsv1.DaemonSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: namespace,
+			Name:      name,
+		},
+	}
+	key := client.ObjectKeyFromObject(ds)
+	existing := &appsv1.DaemonSet{}
+	e := f.GetResource(key, existing)
+	if e != nil {
+		return nil, e
+	}
+	return existing, e
+}
+
+func (f *Framework) WaitDaemonSetReady(name, namespace string, ctx context.Context) (*appsv1.DaemonSet, error) {
+	// l := []client.ListOption{
+	// 	client.InNamespace(namespace),
+	// 	client.MatchingLabels{"app": name},
+	// }
+	// watchInterface, err := f.KClient.Watch(ctx, &appsv1.DeploymentList{}, l...)
+	// l := &client.ListOptions{
+	// 	Namespace:     namespace,
+	// 	LabelSelector: labels.SelectorFromValidatedSet(metav1.),
+	// }
+
+	l := &client.ListOptions{
+		Namespace:     namespace,
+		FieldSelector: fields.OneTermEqualSelector("metadata.name", name),
+	}
+	watchInterface, err := f.KClient.Watch(ctx, &appsv1.DaemonSetList{}, l)
+	if err != nil {
+		return nil, fmt.Errorf("failed to Watch: %v", err)
+	}
+	defer watchInterface.Stop()
+
+	f.t.Logf("pod %v\n", l)
+	f.t.Logf("watchInterface %v\n", watchInterface)
+	if err != nil {
+		return nil, fmt.Errorf("failed to Watch: %v", err)
+	}
+	defer watchInterface.Stop()
+	for {
+		select {
+		case event, ok := <-watchInterface.ResultChan():
+			f.t.Logf("pod check number %v/%v\n", event, ok)
+			if !ok {
+				return nil, fmt.Errorf("channel is closed ")
+			} else {
+				f.t.Logf("daemonset %v/%v %v event \n", namespace, name, event.Type)
+				if event.Type == watch.Error {
+					return nil, fmt.Errorf("received error event: %+v", event)
+				} else if event.Type == watch.Deleted {
+					return nil, fmt.Errorf("resource is deleted")
+				} else {
+					ds, ok := event.Object.(*appsv1.DaemonSet)
+					if !ok {
+						return nil, fmt.Errorf("failed to get metaObject")
+					}
+					f.t.Logf("pod %v/%v status=%+v\n", namespace, name)
+					//already ready Replicas == all Replicas
+					//time.Sleep(20 * time.Second)
+					if ds.Status.CurrentNumberScheduled == ds.Status.DesiredNumberScheduled {
+
+						GinkgoWriter.Printf("CurrentNumberScheduled: %v \n", ds.Status.CurrentNumberScheduled)
+						GinkgoWriter.Printf("DesiredNumberScheduled: %v \n", ds.Status.DesiredNumberScheduled)
+						GinkgoWriter.Printf("ds.Status.NumberReady : %v \n", ds.Status.NumberReady)
+
+						return ds, nil
+					}
+				}
+			}
+
+		case <-ctx.Done():
+			return nil, fmt.Errorf("ctx timeout ")
+		}
+	}
+
+}

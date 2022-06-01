f1ea0d1f4d4e5f37e222300b94c319bd667dd69c
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2022-06-01 12:17:59 +0800

    ci bug
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- tools/scripts/e2eperformance.sh
+++ tools/scripts/e2eperformance.sh
@@ -38,31 +38,39 @@ LoopList::GetElement() {
 
 E2E_DATA=$( cat "$E2E_REPORT_PATH" )
 DATA=$( LoopList::GetElement "$E2E_DATA" "SuiteDescription" '"Performance Suite"' )
-(($?!=0)) && echo "error, failed to get " >&2  && echo "$E2E_DATA" >&2 && exit 1
+(($?!=0)) && echo "error, failed to get SuiteDescription " >&2  && echo "$E2E_DATA" >&2 && exit 1
 
 E2E_DATA=$( jq '.SpecReports' <<< "$DATA" )
 DATA=$( LoopList::GetElement "$E2E_DATA" "LeafNodeLabels"  '["P00002"]'  )
-(($?!=0)) && echo "error, failed to get " >&2  && echo "$E2E_DATA" >&2 && exit 1
+(($?!=0)) && echo "error, failed to get LeafNodeLabels" >&2  && echo "$E2E_DATA" >&2 && exit 1
+
+
+CASE_STATE=$( jq '.State' <<< "$DATA" | tr -d '"' )
+(($?!=0)) && echo "error, failed to get case status " >&2  && echo "$DATA" >&2 && exit 1
+if [ "$CASE_STATE" != "passed" ] ; then
+    echo "the state of performance test case P00002 is $CASE_STATE, ignore retrieve result" >&2
+    exit 0
+fi
 
 E2E_DATA=$( jq '.ReportEntries' <<< "$DATA" )
 DATA=$( LoopList::GetElement "$E2E_DATA" "Name"  '"Performance Results"'  )
-(($?!=0)) && echo "error, failed to get " >&2 && echo "$E2E_DATA" >&2 && exit 1
+(($?!=0)) && echo "error, failed to get ReportEntries" >&2 && echo "$E2E_DATA" >&2 && exit 1
 
 ENTRY_DATA=$( jq '.Value.Representation' <<< "$DATA" | sed 's/\\//g')
-(($?!=0)) && echo "error, failed to get " >&2 && echo "$ENTRY_DATA" >&2 && exit 1
+(($?!=0)) && echo "error, failed to get Representation" >&2 && echo "$ENTRY_DATA" >&2 && exit 1
 
 # "{ "controllerType" : "deployment", "replicas": 60, "createTime": 60 , "rebuildTime": 100, "deleteTime": 48 }"
 # to
 # { "controllerType" : "deployment", "replicas": 60, "createTime": 60 , "rebuildTime": 100, "deleteTime": 48 }
 ENTRY_DATA=$( echo "$ENTRY_DATA" | sed 's/"{/{/' | sed 's/}"/}/' )
-(($?!=0)) && echo "error, failed to get " >&2  && echo "$ENTRY_DATA" && exit 1
+(($?!=0)) && echo "error, failed to get data" >&2  && echo "$ENTRY_DATA" >&2 && exit 1
 
 createTime=$( jq '.createTime' <<< "$ENTRY_DATA" )
 rebuildTime=$( jq '.rebuildTime' <<< "$ENTRY_DATA" )
 deleteTime=$( jq '.deleteTime' <<< "$ENTRY_DATA" )
 
-[ -z "$createTime" ] && echo "error, failed to get createTime " >&2  && echo "$ENTRY_DATA" && exit 1
-[ -z "$rebuildTime" ] && echo "error, failed to get rebuildTime " >&2  && echo "$ENTRY_DATA" && exit 1
-[ -z "$deleteTime" ] && echo "error, failed to get deleteTime " >&2  && echo "$ENTRY_DATA" && exit 1
+[ -z "$createTime" ] && echo "error, failed to get createTime " >&2  && echo "$ENTRY_DATA" >&2 && exit 1
+[ -z "$rebuildTime" ] && echo "error, failed to get rebuildTime " >&2  && echo "$ENTRY_DATA" >&2 && exit 1
+[ -z "$deleteTime" ] && echo "error, failed to get deleteTime " >&2  && echo "$ENTRY_DATA" >&2 && exit 1
 
 echo "${createTime}/${rebuildTime}/${deleteTime}"

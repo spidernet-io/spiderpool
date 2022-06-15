34fe9a103e095f9cc710e1c76a16c49a76df7497
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2022-06-15 20:33:27 +0800

    e2e: add ippool pkg
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- test/e2e/assignip/assiginip_suite_test.go
+++ test/e2e/assignip/assiginip_suite_test.go
@@ -9,8 +9,8 @@ import (
 	. "github.com/onsi/gomega"
 	e2e "github.com/spidernet-io/e2eframework/framework"
 	spiderpool "github.com/spidernet-io/spiderpool/pkg/k8s/apis/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"github.com/spidernet-io/spiderpool/test/e2e/common"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 func TestAssignIP(t *testing.T) {
@@ -29,5 +29,10 @@ var _ = BeforeSuite(func() {
 
 	ClusterDefaultV4Ipool, ClusterDefaultV6Ipool, e = common.GetClusterDefaultIppool(frame)
 	Expect(e).NotTo(HaveOccurred())
-
+	if frame.Info.IpV4Enabled && len(ClusterDefaultV4Ipool) == 0 {
+		Fail("failed to find cluster ipv4 ippool")
+	}
+	if frame.Info.IpV6Enabled && len(ClusterDefaultV6Ipool) == 0 {
+		Fail("failed to find cluster ipv6 ippool")
+	}
 })

8d549f5f577591b0959b8348a9202efb90dbb764
bzsuni <bingzhe.sun@daocloud.io>
2022-07-07 17:28:50 +0800

    fail to run pod who is bound to an ippool set with no-matched selector

--- test/e2e/labelselector/labelselector_suite_test.go
+++ test/e2e/labelselector/labelselector_suite_test.go
@@ -3,13 +3,14 @@
 package labelselector_test
 
 import (
+	e2e "github.com/spidernet-io/e2eframework/framework"
+	spiderpool "github.com/spidernet-io/spiderpool/pkg/k8s/apis/v1"
+	"github.com/spidernet-io/spiderpool/test/e2e/common"
+	"k8s.io/apimachinery/pkg/runtime"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	e2e "github.com/spidernet-io/e2eframework/framework"
-	spiderpool "github.com/spidernet-io/spiderpool/pkg/k8s/apis/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 )
 
 func TestLabelSelector(t *testing.T) {
@@ -18,11 +19,20 @@ func TestLabelSelector(t *testing.T) {
 }
 
 var frame *e2e.Framework
+var ClusterDefaultV4IpoolList, ClusterDefaultV6IpoolList []string
 
 var _ = BeforeSuite(func() {
 	defer GinkgoRecover()
 	var e error
 	frame, e = e2e.NewFramework(GinkgoT(), []func(*runtime.Scheme) error{spiderpool.AddToScheme})
-
 	Expect(e).NotTo(HaveOccurred())
+
+	ClusterDefaultV4IpoolList, ClusterDefaultV6IpoolList, e = common.GetClusterDefaultIppool(frame)
+	Expect(e).NotTo(HaveOccurred())
+	if frame.Info.IpV4Enabled && len(ClusterDefaultV4IpoolList) == 0 {
+		Fail("failed to find cluster ipv4 ippool")
+	}
+	if frame.Info.IpV6Enabled && len(ClusterDefaultV6IpoolList) == 0 {
+		Fail("failed to find cluster ipv6 ippool")
+	}
 })

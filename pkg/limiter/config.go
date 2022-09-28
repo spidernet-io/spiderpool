2322c97193a438b913ff6beb413f96c40762429f
iiiceoo <ziqian.xue@daocloud.io>
2022-09-28 21:43:52 +0800

    Set defaults for limiter config
    Signed-off-by: iiiceoo <ziqian.xue@daocloud.io>

--- pkg/limiter/config.go
+++ pkg/limiter/config.go
@@ -5,7 +5,25 @@ package limiter
 
 import "time"
 
+const (
+	defaultMaxQueueSize = 1000
+	defaultMaxWaitTime  = 15 * time.Second
+)
+
 type LimiterConfig struct {
-	MaxQueueSize int
-	MaxWaitTime  time.Duration
+	MaxQueueSize *int
+	MaxWaitTime  *time.Duration
+}
+
+func setDefaultsForLimiterConfig(config LimiterConfig) LimiterConfig {
+	maxQueueSize, maxWaitTime := defaultMaxQueueSize, defaultMaxWaitTime
+	if config.MaxQueueSize == nil {
+		config.MaxQueueSize = &maxQueueSize
+	}
+
+	if config.MaxWaitTime == nil {
+		config.MaxWaitTime = &maxWaitTime
+	}
+
+	return config
 }

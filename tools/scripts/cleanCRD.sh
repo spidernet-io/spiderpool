b3eb69c4ca35cd19080392d2353086d5c6453bb3
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2022-09-29 00:15:05 +0800

    clean crd script
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

---
+++ tools/scripts/cleanCRD.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+kubectl get spiderippools | sed '1 d' | awk '{print $1}' | xargs -n 1 -i kubectl patch spiderippools {} --patch '{"metadata": {"finalizers": null}}' --type=merge
+
+kubectl get spidersubnet | sed '1 d' | awk '{print $1}' | xargs -n 1 -i kubectl patch spidersubnet {} --patch '{"metadata": {"finalizers": null}}' --type=merge
+
+ALL_EP_INFO=`kubectl get spiderendpoints -A | sed '1 d'`
+while read namespace epName other ; do
+  kubectl patch -n $namespace $epName --patch '{"metadata": {"finalizers": null}}' --type=merge
+done <<< "$ALL_EP_INFO"
+
+kubectl delete crd spiderippools
+kubectl delete crd spiderreservedips
+kubectl delete crd spiderendpoints
+kubectl delete crd spidersubnets

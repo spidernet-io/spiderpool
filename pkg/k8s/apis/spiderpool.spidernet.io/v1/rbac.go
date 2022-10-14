24117f223d2c499b04ecd93c784ba944600f0b3b
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2022-10-14 20:50:47 +0800

    remove role for secret
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
    
    d
    
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- pkg/k8s/apis/spiderpool.spidernet.io/v1/rbac.go
+++ pkg/k8s/apis/spiderpool.spidernet.io/v1/rbac.go
@@ -8,11 +8,10 @@
 // +kubebuilder:rbac:groups=spiderpool.spidernet.io,resources=spiderendpoints,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=spiderpool.spidernet.io,resources=spiderendpoints/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=spiderpool.spidernet.io,resources=spiderreservedips,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups="",resources=nodes;namespaces;endpoints;pods;secrets,verbs=get;list;watch;update
 // +kubebuilder:rbac:groups="",resources=events,verbs=create;get;list;watch;update;delete
 // +kubebuilder:rbac:groups="coordination.k8s.io",resources=leases,verbs=create;get;update
 // +kubebuilder:rbac:groups="apps",resources=statefulsets;deployments;replicasets;daemonsets,verbs=get;list;update;watch
 // +kubebuilder:rbac:groups="batch",resources=jobs;cronjobs,verbs=get;list;update;watch
-// +kubebuilder:rbac:groups="admissionregistration.k8s.io",resources=mutatingwebhookconfigurations;validatingwebhookconfigurations,verbs=get;list;update;watch;patch
+// +kubebuilder:rbac:groups="",resources=nodes;namespaces;endpoints;pods,verbs=get;list;watch;update
 
 package v1

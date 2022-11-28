a7beae66ab5415750ccf2ab96c6a8f8a7a148b64
ty-dc <tao.yang@daocloud.io>
2022-11-28 16:25:52 +0800

    unittest: add some nittest
    Signed-off-by: ty-dc <tao.yang@daocloud.io>

---
+++ pkg/nodemanager/nodemanager_suite_test.go
@@ -0,0 +1,16 @@
+// Copyright 2022 Authors of spidernet-io
+// SPDX-License-Identifier: Apache-2.0
+
+package nodemanager_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestNodemanager(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Nodemanager Suite")
+}
---
+++ pkg/podmanager/pod_manager_test.go
@@ -0,0 +1,12 @@
+// Copyright 2022 Authors of spidernet-io
+// SPDX-License-Identifier: Apache-2.0
+
+package podmanager_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var _ = Describe("Podmanager", Label("unittest", "podmanager_test"), func() {
+
+})

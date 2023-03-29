48235d921f75f65fc28493680572ee7a34dc2d4d
iiiceoo <ziqian.xue@daocloud.io>
2023-03-29 18:44:52 +0800

    test: Add some unit tests for IPPoolManager
    Signed-off-by: iiiceoo <ziqian.xue@daocloud.io>

--- pkg/ippoolmanager/ippool_manager_test.go
+++ pkg/ippoolmanager/ippool_manager_test.go
@@ -6,20 +6,27 @@ package ippoolmanager_test
 import (
 	"context"
 	"fmt"
+	"net"
 	"sync/atomic"
 
 	"github.com/agiledragon/gomonkey/v2"
+	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/uuid"
 	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/spidernet-io/spiderpool/pkg/constant"
 	"github.com/spidernet-io/spiderpool/pkg/ippoolmanager"
 	spiderpoolv2beta1 "github.com/spidernet-io/spiderpool/pkg/k8s/apis/spiderpool.spidernet.io/v2beta1"
+	spiderpooltypes "github.com/spidernet-io/spiderpool/pkg/types"
+	"github.com/spidernet-io/spiderpool/pkg/utils/convert"
 )
 
 var _ = Describe("IPPoolManager", Label("ippool_manager_test"), func() {
@@ -231,6 +238,314 @@ var _ = Describe("IPPoolManager", Label("ippool_manager_test"), func() {
 					}
 				}
 				Expect(hasIPPool).To(BeTrue())
+			})
+		})
+
+		Describe("AllocateIP", func() {
+			var nic string
+			var podT *corev1.Pod
+
+			BeforeEach(func() {
+				nic = "eth0"
+				podT = &corev1.Pod{
+					TypeMeta: metav1.TypeMeta{
+						Kind:       "Pod",
+						APIVersion: corev1.SchemeGroupVersion.String(),
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "pod",
+						Namespace: "default",
+						UID:       uuid.NewUUID(),
+					},
+					Spec: corev1.PodSpec{},
+				}
+			})
+
+			It("allocate IP address from non-existent IPPool", func() {
+				res, err := ipPoolManager.AllocateIP(ctx, ipPoolName, nic, podT)
+				Expect(apierrors.IsNotFound(err)).To(BeTrue())
+				Expect(res).To(BeNil())
+			})
+
+			It("failed to assemble the reserved IP addresses due to some unknown errors", func() {
+				mockRIPManager.EXPECT().
+					AssembleReservedIPs(gomock.Eq(ctx), gomock.Eq(constant.IPv4)).
+					Return(nil, constant.ErrUnknown).
+					Times(1)
+
+				ipPoolT.Spec.IPVersion = pointer.Int64(constant.IPv4)
+				ipPoolT.Spec.IPs = append(ipPoolT.Spec.IPs, "172.18.40.40")
+
+				err := fakeClient.Create(ctx, ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+				err = tracker.Add(ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+
+				res, err := ipPoolManager.AllocateIP(ctx, ipPoolName, nic, podT)
+				Expect(err).To(MatchError(constant.ErrUnknown))
+				Expect(res).To(BeNil())
+			})
+
+			It("failed to update IPPool due to some unknown errors", func() {
+				mockRIPManager.EXPECT().
+					AssembleReservedIPs(gomock.Eq(ctx), gomock.Eq(constant.IPv4)).
+					Return(nil, nil).
+					Times(1)
+
+				patches := gomonkey.ApplyMethodReturn(fakeClient, "Update", constant.ErrUnknown)
+				defer patches.Reset()
+
+				ipPoolT.Spec.IPVersion = pointer.Int64(constant.IPv4)
+				ipPoolT.Spec.IPs = append(ipPoolT.Spec.IPs, "172.18.40.40")
+
+				err := fakeClient.Create(ctx, ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+				err = tracker.Add(ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+
+				res, err := ipPoolManager.AllocateIP(ctx, ipPoolName, nic, podT)
+				Expect(err).To(MatchError(constant.ErrUnknown))
+				Expect(res).To(BeNil())
+			})
+
+			It("runs out of retries to update IPPool, but conflicts still occur", func() {
+				mockRIPManager.EXPECT().
+					AssembleReservedIPs(gomock.Eq(ctx), gomock.Eq(constant.IPv4)).
+					Return(nil, nil).
+					Times(5)
+
+				patches := gomonkey.ApplyMethodReturn(fakeClient, "Update", apierrors.NewConflict(schema.GroupResource{Resource: "test"}, "other", nil))
+				defer patches.Reset()
+
+				ipPoolT.Spec.IPVersion = pointer.Int64(constant.IPv4)
+				ipPoolT.Spec.IPs = append(ipPoolT.Spec.IPs, "172.18.40.40")
+
+				err := fakeClient.Create(ctx, ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+				err = tracker.Add(ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+
+				res, err := ipPoolManager.AllocateIP(ctx, ipPoolName, nic, podT)
+				Expect(err).To(MatchError(constant.ErrRetriesExhausted))
+				Expect(res).To(BeNil())
+			})
+
+			It("allocate IP address", func() {
+				mockRIPManager.EXPECT().
+					AssembleReservedIPs(gomock.Eq(ctx), gomock.Eq(constant.IPv4)).
+					Return(nil, nil).
+					Times(1)
+
+				ipVersion := constant.IPv4
+				allocatedIP := "172.18.40.40/24"
+				gateway := "172.18.40.1"
+				vlan := int64(0)
+
+				ip, ipNet, err := net.ParseCIDR(allocatedIP)
+				Expect(err).NotTo(HaveOccurred())
+
+				ipPoolT.Spec.IPVersion = pointer.Int64(ipVersion)
+				ipPoolT.Spec.Subnet = ipNet.String()
+				ipPoolT.Spec.IPs = append(ipPoolT.Spec.IPs, ip.String())
+				ipPoolT.Spec.Gateway = pointer.String(gateway)
+				ipPoolT.Spec.Vlan = pointer.Int64(vlan)
+
+				err = fakeClient.Create(ctx, ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+				err = tracker.Add(ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+
+				res, err := ipPoolManager.AllocateIP(ctx, ipPoolName, nic, podT)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(*res.Nic).To(Equal(nic))
+				Expect(*res.Version).To(Equal(ipVersion))
+				Expect(*res.Address).To(Equal(allocatedIP))
+				Expect(res.IPPool).To(Equal(ipPoolT.Name))
+				Expect(res.Gateway).To(Equal(gateway))
+				Expect(res.Vlan).To(Equal(vlan))
+			})
+		})
+
+		Describe("ReleaseIP", func() {
+			var ip string
+			var uid string
+			var records spiderpoolv2beta1.PoolIPAllocations
+
+			BeforeEach(func() {
+				ip = "172.18.40.40"
+				uid = string(uuid.NewUUID())
+				records = spiderpoolv2beta1.PoolIPAllocations{
+					ip: spiderpoolv2beta1.PoolIPAllocation{
+						NIC:            "eth0",
+						NamespacedName: "default/pod",
+						PodUID:         uid,
+					},
+				}
+			})
+
+			It("release IP record from non-existent IPPool", func() {
+				err := ipPoolManager.ReleaseIP(ctx, ipPoolName, []spiderpooltypes.IPAndUID{})
+				Expect(apierrors.IsNotFound(err)).To(BeTrue())
+			})
+
+			It("release the IP record with unmatched Pod UID", func() {
+				data, err := convert.MarshalIPPoolAllocatedIPs(records)
+				Expect(err).NotTo(HaveOccurred())
+
+				ipPoolT.Status.AllocatedIPs = data
+				err = tracker.Add(ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+
+				err = ipPoolManager.ReleaseIP(ctx, ipPoolName, []spiderpooltypes.IPAndUID{{IP: ip, UID: string(uuid.NewUUID())}})
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("failed to update IPPool due to some unknown errors", func() {
+				patches := gomonkey.ApplyMethodReturn(fakeClient, "Update", constant.ErrUnknown)
+				defer patches.Reset()
+
+				data, err := convert.MarshalIPPoolAllocatedIPs(records)
+				Expect(err).NotTo(HaveOccurred())
+
+				ipPoolT.Status.AllocatedIPs = data
+				err = fakeClient.Create(ctx, ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+				err = tracker.Add(ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+
+				err = ipPoolManager.ReleaseIP(ctx, ipPoolName, []spiderpooltypes.IPAndUID{{IP: ip, UID: uid}})
+				Expect(err).To(MatchError(constant.ErrUnknown))
+			})
+
+			It("runs out of retries to update IPPool, but conflicts still occur", func() {
+				patches := gomonkey.ApplyMethodReturn(fakeClient, "Update", apierrors.NewConflict(schema.GroupResource{Resource: "test"}, "other", nil))
+				defer patches.Reset()
+
+				data, err := convert.MarshalIPPoolAllocatedIPs(records)
+				Expect(err).NotTo(HaveOccurred())
+
+				ipPoolT.Status.AllocatedIPs = data
+				err = fakeClient.Create(ctx, ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+				err = tracker.Add(ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+
+				err = ipPoolManager.ReleaseIP(ctx, ipPoolName, []spiderpooltypes.IPAndUID{{IP: ip, UID: uid}})
+				Expect(err).To(MatchError(constant.ErrRetriesExhausted))
+			})
+
+			It("release the IP record", func() {
+				data, err := convert.MarshalIPPoolAllocatedIPs(records)
+				Expect(err).NotTo(HaveOccurred())
+
+				ipPoolT.Status.AllocatedIPs = data
+				err = fakeClient.Create(ctx, ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+				err = tracker.Add(ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+
+				err = ipPoolManager.ReleaseIP(ctx, ipPoolName, []spiderpooltypes.IPAndUID{{IP: ip, UID: uid}})
+				Expect(err).NotTo(HaveOccurred())
+
+				var ipPool spiderpoolv2beta1.SpiderIPPool
+				err = fakeClient.Get(ctx, types.NamespacedName{Name: ipPoolT.Name}, &ipPool)
+				Expect(err).NotTo(HaveOccurred())
+
+				newRecords, err := convert.UnmarshalIPPoolAllocatedIPs(ipPool.Status.AllocatedIPs)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(newRecords).To(BeEmpty())
+			})
+		})
+
+		Describe("UpdateAllocatedIPs", func() {
+			var ip string
+			var uid string
+			var records spiderpoolv2beta1.PoolIPAllocations
+
+			BeforeEach(func() {
+				ip = "172.18.40.40"
+				uid = string(uuid.NewUUID())
+				records = spiderpoolv2beta1.PoolIPAllocations{
+					ip: spiderpoolv2beta1.PoolIPAllocation{
+						NIC:            "eth0",
+						NamespacedName: "default/pod",
+						PodUID:         uid,
+					},
+				}
+			})
+
+			It("updates the allocated IP record from non-existent IPPool", func() {
+				err := ipPoolManager.UpdateAllocatedIPs(ctx, ipPoolName, []spiderpooltypes.IPAndUID{})
+				Expect(apierrors.IsNotFound(err)).To(BeTrue())
+			})
+
+			It("updates the allocated IP record for Pod that have not been recreated", func() {
+				data, err := convert.MarshalIPPoolAllocatedIPs(records)
+				Expect(err).NotTo(HaveOccurred())
+
+				ipPoolT.Status.AllocatedIPs = data
+				err = tracker.Add(ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+
+				err = ipPoolManager.UpdateAllocatedIPs(ctx, ipPoolName, []spiderpooltypes.IPAndUID{{IP: ip, UID: uid}})
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("failed to update IPPool due to some unknown errors", func() {
+				patches := gomonkey.ApplyMethodReturn(fakeClient, "Update", constant.ErrUnknown)
+				defer patches.Reset()
+
+				data, err := convert.MarshalIPPoolAllocatedIPs(records)
+				Expect(err).NotTo(HaveOccurred())
+
+				ipPoolT.Status.AllocatedIPs = data
+				err = fakeClient.Create(ctx, ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+				err = tracker.Add(ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+
+				err = ipPoolManager.UpdateAllocatedIPs(ctx, ipPoolName, []spiderpooltypes.IPAndUID{{IP: ip, UID: string(uuid.NewUUID())}})
+				Expect(err).To(MatchError(constant.ErrUnknown))
+			})
+
+			It("runs out of retries to update IPPool, but conflicts still occur", func() {
+				patches := gomonkey.ApplyMethodReturn(fakeClient, "Update", apierrors.NewConflict(schema.GroupResource{Resource: "test"}, "other", nil))
+				defer patches.Reset()
+
+				data, err := convert.MarshalIPPoolAllocatedIPs(records)
+				Expect(err).NotTo(HaveOccurred())
+
+				ipPoolT.Status.AllocatedIPs = data
+				err = fakeClient.Create(ctx, ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+				err = tracker.Add(ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+
+				err = ipPoolManager.UpdateAllocatedIPs(ctx, ipPoolName, []spiderpooltypes.IPAndUID{{IP: ip, UID: string(uuid.NewUUID())}})
+				Expect(err).To(MatchError(constant.ErrRetriesExhausted))
+			})
+
+			It("updates the allocated IP record", func() {
+				data, err := convert.MarshalIPPoolAllocatedIPs(records)
+				Expect(err).NotTo(HaveOccurred())
+
+				ipPoolT.Status.AllocatedIPs = data
+				err = fakeClient.Create(ctx, ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+				err = tracker.Add(ipPoolT)
+				Expect(err).NotTo(HaveOccurred())
+
+				newUID := string(uuid.NewUUID())
+				err = ipPoolManager.UpdateAllocatedIPs(ctx, ipPoolName, []spiderpooltypes.IPAndUID{{IP: ip, UID: newUID}})
+				Expect(err).NotTo(HaveOccurred())
+
+				var ipPool spiderpoolv2beta1.SpiderIPPool
+				err = fakeClient.Get(ctx, types.NamespacedName{Name: ipPoolT.Name}, &ipPool)
+				Expect(err).NotTo(HaveOccurred())
+
+				newRecords, err := convert.UnmarshalIPPoolAllocatedIPs(ipPool.Status.AllocatedIPs)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(newRecords[ip].PodUID).To(Equal(newUID))
 			})
 		})
 	})

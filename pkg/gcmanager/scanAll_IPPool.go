228bed53d72491c1cc9cc93a047d8401e21a40ec
Icarus9913 <icaruswu66@qq.com>
2023-06-06 16:33:24 +0800

    improve log print
    Signed-off-by: Icarus9913 <icaruswu66@qq.com>

--- pkg/gcmanager/scanAll_IPPool.go
+++ pkg/gcmanager/scanAll_IPPool.go
@@ -26,7 +26,9 @@ import (
 func (s *SpiderGC) monitorGCSignal(ctx context.Context) {
 	logger.Debug("start to monitor gc signal for CLI or default GC interval")
 
-	timer := time.NewTimer(time.Duration(s.gcConfig.DefaultGCIntervalDuration) * time.Second)
+	d := time.Duration(s.gcConfig.DefaultGCIntervalDuration) * time.Second
+	logger.Sugar().Debugf("default IP GC interval duration is %v", d)
+	timer := time.NewTimer(d)
 	defer timer.Stop()
 
 	logger.Debug("initial scan all for cluster firstly")
@@ -61,7 +63,7 @@ func (s *SpiderGC) monitorGCSignal(ctx context.Context) {
 			}
 
 		case <-ctx.Done():
-			logger.Info("receive ctx done, stop monitoring gc signal!")
+			logger.Warn("receive ctx done, stop monitoring gc signal!")
 			return
 		}
 

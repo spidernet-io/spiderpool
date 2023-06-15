843fbafff125914a2a4f93dc6d351032897e068d
cyclinder <qifeng.guo@daocloud.io>
2023-06-15 16:47:05 +0800

    feat: add cni plugins ifacer

---
+++ cmd/ifacer/cmd/cmd_del.go
@@ -0,0 +1,10 @@
+// Copyright 2023 Authors of spidernet-io
+// SPDX-License-Identifier: Apache-2.0
+
+package cmd
+
+import "github.com/containernetworking/cni/pkg/skel"
+
+func CmdDel(args *skel.CmdArgs) error {
+	return nil
+}

853f7930a906f9d0a8128e39ba2053ec9a263a6f
Hobbey W <hobbey12@gmail.com>
2023-08-04 15:15:54 +0800

    fix:Missing resource type in cleanCRD scripts (#2131)
    spidercoordinators & spidermultusconfigs

--- tools/scripts/cleanCRD.sh
+++ tools/scripts/cleanCRD.sh
@@ -7,6 +7,8 @@ kubectl get spiderippools | sed '1 d' | awk '{print $1}' | xargs -n 1 -i kubectl
 
 kubectl get spidersubnets | sed '1 d' | awk '{print $1}' | xargs -n 1 -i kubectl patch spidersubnets {} --patch '{"metadata": {"finalizers": null}}' --type=merge
 
+kubectl get spidercoordinators | sed '1 d' | awk '{print $1}' | xargs -n 1 -i kubectl patch spidercoordinators {} --patch '{"metadata": {"finalizers": null}}' --type=merge
+
 ALL_EP_INFO=`kubectl get spiderendpoints -A | sed '1 d'`
 while read namespace epName other ; do
   kubectl patch -n $namespace spiderendpoints $epName --patch '{"metadata": {"finalizers": null}}' --type=merge
@@ -16,3 +18,5 @@ kubectl delete crd spiderendpoints.spiderpool.spidernet.io
 kubectl delete crd spiderippools.spiderpool.spidernet.io
 kubectl delete crd spiderreservedips.spiderpool.spidernet.io
 kubectl delete crd spidersubnets.spiderpool.spidernet.io
+kubectl delete crd spidercoordinators.spiderpool.spidernet.io
+kubectl delete crd spidermultusconfigs.spiderpool.spidernet.io

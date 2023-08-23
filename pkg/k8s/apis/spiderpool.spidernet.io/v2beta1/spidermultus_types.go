830ceca4d55d92753faf822a4791086cbd8d9bd9
Cyclinder <qifeng.guo@daocloud.io>
2023-08-23 10:43:17 +0800

    spidermultusconfig: ovs cni config supported (#2199)

--- pkg/k8s/apis/spiderpool.spidernet.io/v2beta1/spidermultus_types.go
+++ pkg/k8s/apis/spiderpool.spidernet.io/v2beta1/spidermultus_types.go
@@ -28,7 +28,7 @@ type SpiderMultusConfigList struct {
 // MultusCNIConfigSpec defines the desired state of SpiderMultusConfig.
 type MultusCNIConfigSpec struct {
 	// +kubebuilder:validation:Required
-	// +kubebuilder:validation:Enum=macvlan;ipvlan;sriov;custom
+	// +kubebuilder:validation:Enum=macvlan;ipvlan;sriov;ovs;custom
 	CniType string `json:"cniType"`
 
 	// +kubebuilder:validation:Optional
@@ -39,6 +39,9 @@ type MultusCNIConfigSpec struct {
 
 	// +kubebuilder:validation:Optional
 	SriovConfig *SpiderSRIOVCniConfig `json:"sriov,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	OvsConfig *SpiderOvsCniConfig `json:"ovs,omitempty"`
 
 	// +kubebuilder:default=true
 	// +kubebuilder:validation:Optional
@@ -95,6 +98,35 @@ type SpiderSRIOVCniConfig struct {
 
 	// +kubebuilder:validation:Optional
 	SpiderpoolConfigPools *SpiderpoolPools `json:"ippools,omitempty"`
+}
+
+type SpiderOvsCniConfig struct {
+	// +kubebuilder:validation:Required
+	BrName string `json:"bridge"`
+	// +kubebuilder:validation:Optional
+	VlanTag *int32 `json:"vlan"`
+	// +kubebuilder:validation:Optional
+	Trunk []*Trunk `json:"trunk,omitempty"`
+	// +kubebuilder:validation:Optional
+	// PCI address of a VF in valid sysfs format
+	DeviceID string `json:"deviceID"`
+	// +kubebuilder:validation:Optional
+	SpiderpoolConfigPools *SpiderpoolPools `json:"ippools,omitempty"`
+}
+
+type Trunk struct {
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Maximum=4094
+	MinID *uint `json:"minID,omitempty"`
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Maximum=4094
+	MaxID *uint `json:"maxID,omitempty"`
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Maximum=4094
+	ID *uint `json:"id,omitempty"`
 }
 
 type BondConfig struct {

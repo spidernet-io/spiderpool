e5fae0ef2899968fde46575aaac24e837d51710c
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2023-09-01 11:15:02 +0800

    fix: changelog
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- tools/scripts/changelog.sh
+++ tools/scripts/changelog.sh
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # usage:
-#  GH_TOKEN=${{ github.token }} LABEL_FEATURE="release/feature-new" LABEL_BUG="release/bug" PROJECT_REPO="spidernet-io/spiderpool" changelog.sh ./ v0.3.6  "spidernet-io/spiderpool"
-#  GH_TOKEN=${{ github.token }} LABEL_FEATURE="release/feature-new" LABEL_BUG="release/bug" PROJECT_REPO="spidernet-io/spiderpool" changelog.sh ./ v0.3.6 v0.3.5  "spidernet-io/spiderpool"
+#  GH_TOKEN=${{ github.token }} LABEL_FEATURE="release/feature-new" LABEL_CHANGED="release/feature-changed" LABEL_BUG="release/bug" PROJECT_REPO="xxx/xxx" changelog.sh  ./ v0.3.6
+#  GH_TOKEN=${{ github.token }} LABEL_FEATURE="release/feature-new" LABEL_CHANGED="release/feature-changed" LABEL_BUG="release/bug" PROJECT_REPO="xxx/xxx" changelog.sh  ./ v0.3.6 v0.3.5
 
 
 CURRENT_DIR_PATH=$(cd `dirname $0`; pwd)
@@ -16,24 +16,29 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-#required
+# optional
 OUTPUT_DIR=${1}
-#required
+# required
 DEST_TAG=${2}
 # optional
 START_TAG=${3:-""}
-GITHUB_REPO=${4:-"spidernet-io/spiderpool"}
-
-[ -n "${GH_TOKEN}" ] || { echo "error, miss GH_TOKEN"; exit 1 ; }
 
 
 LABEL_FEATURE=${LABEL_FEATURE:-"release/feature-new"}
+LABEL_CHANGED=${LABEL_BUG:-"release/feature-changed"}
 LABEL_BUG=${LABEL_BUG:-"release/bug"}
-PROJECT_REPO=${PROJECT_REPO:-"spidernet-io/spiderpool"}
+PROJECT_REPO=${PROJECT_REPO:-""}
+[ -n "$PROJECT_REPO" ] || { echo "miss PROJECT_REPO"; exit 1 ; }
+[ -n "${GH_TOKEN}" ] || { echo "error, miss GH_TOKEN"; exit 1 ; }
 
+( cd ${OUTPUT_DIR} ) || { echo "error, OUTPUT_DIR '${OUTPUT_DIR}' is not a valid directory" ; exit 1 ; }
 OUTPUT_DIR=$(cd ${OUTPUT_DIR}; pwd)
 echo "generate changelog to directory ${OUTPUT_DIR}"
-cd ${OUTPUT_DIR}
+
+# change to root for git cli
+cd ${PROJECT_ROOT_PATH}
+
+#============================
 
 ORIGIN_START_TAG=${START_TAG}
 if [ -z "${START_TAG}" ] ; then
@@ -116,18 +121,20 @@ TOTAL_COUNT="0"
 PR_LIST=""
 #
 FEATURE_PR=""
+CHANGED_PR=""
 FIX_PR=""
 for COMMIT in ${ALL_COMMIT} ; do
   # API RATE LIMIT
   # https://docs.github.com/en/rest/overview/resources-in-the-rest-api?apiVersion=2022-11-28#rate-limiting
   # When using GITHUB_TOKEN, the rate limit is 1,000 requests per hour per repository
   # GitHub Enterprise Cloud's rate limit applies, and the limit is 15,000 requests per hour per repository.
-  PR=` curl -s -H "Accept: application/vnd.github.groot-preview+json" -H "Authorization: Bearer ${GH_TOKEN}" https://api.github.com/repos/${GITHUB_REPO}/commits/${COMMIT}/pulls | jq -r '.[].number' `
-  [ -n "${PR}" ] || { echo "error, failed to find PR number for commit ${COMMIT} " ; continue ; }
+  COMMIT_INFO=` curl -s -H "Accept: application/vnd.github.groot-preview+json" -H "Authorization: Bearer ${GH_TOKEN}" https://api.github.com/repos/${PROJECT_REPO}/commits/${COMMIT}/pulls `
+  PR=` jq -r '.[].number' <<< "${COMMIT_INFO}" `
+  [ -n "${PR}" ] || { echo "warning, failed to find PR number for commit ${COMMIT} " ; echo "${COMMIT_INFO}" ; echo "" ; continue ; }
   if grep " ${PR} " <<< " ${PR_LIST} " &>/dev/null ; then
-      continue
+    continue
   else
-      PR_LIST+=" ${PR} "
+    PR_LIST+=" ${PR} "
   fi
   (( TOTAL_COUNT+=1 ))
 	INFO=` gh pr view ${PR}  `
@@ -137,9 +144,15 @@ for COMMIT in ${ALL_COMMIT} ; do
 	PR_URL="https://github.com/${PROJECT_REPO}/pull/${PR}"
 	#
 	if grep -E "[[:space:]]${LABEL_FEATURE}[[:space:]]" <<< " ${LABELS} " &>/dev/null ; then
+	  echo "get new feature PR ${PR}"
 		FEATURE_PR+="* ${TITLE} : [PR ${PR}](${PR_URL})
 "
+	elif grep -E "[[:space:]]${LABEL_CHANGED}[[:space:]]" <<< " ${LABELS} " &>/dev/null ; then
+	  echo "get changed feature PR ${PR}"
+		CHANGED_PR+="* ${TITLE} : [PR ${PR}](${PR_URL})
+"
 	elif grep -E "[[:space:]]${LABEL_BUG}[[:space:]]" <<< " ${LABELS} " &>/dev/null ; then
+	  echo "get bug fix PR ${PR}"
 		FIX_PR+="* ${TITLE} : [PR ${PR}](${PR_URL})
 "
 	fi
@@ -154,12 +167,23 @@ echo "***" >> ${FILE_CHANGELOG}
 echo "" >> ${FILE_CHANGELOG}
 #
 if [ -n "${FEATURE_PR}" ]; then
-    echo "## Feature" >> ${FILE_CHANGELOG}
+    echo "## New Feature" >> ${FILE_CHANGELOG}
     echo "" >> ${FILE_CHANGELOG}
     while read LINE ; do
       echo "${LINE}" >> ${FILE_CHANGELOG}
       echo "" >> ${FILE_CHANGELOG}
     done <<< "${FEATURE_PR}"
+    echo "***" >> ${FILE_CHANGELOG}
+    echo "" >> ${FILE_CHANGELOG}
+fi
+#
+if [ -n "${CHANGED_PR}" ]; then
+    echo "## Changed Feature" >> ${FILE_CHANGELOG}
+    echo "" >> ${FILE_CHANGELOG}
+    while read LINE ; do
+      echo "${LINE}" >> ${FILE_CHANGELOG}
+      echo "" >> ${FILE_CHANGELOG}
+    done <<< "${CHANGED_PR}"
     echo "***" >> ${FILE_CHANGELOG}
     echo "" >> ${FILE_CHANGELOG}
 fi
@@ -175,8 +199,10 @@ if [ -n "${FIX_PR}" ]; then
     echo "" >> ${FILE_CHANGELOG}
 fi
 #
-echo "## Totoal PR" >> ${FILE_CHANGELOG}
+echo "## Totoal " >> ${FILE_CHANGELOG}
 echo "" >> ${FILE_CHANGELOG}
-echo "[ ${TOTAL_COUNT} PR](https://github.com/${PROJECT_REPO}/compare/${START_TAG}...${DEST_TAG})" >> ${FILE_CHANGELOG}
+echo "Pull request number: ${TOTAL_COUNT}" >> ${FILE_CHANGELOG}
+echo "" >> ${FILE_CHANGELOG}
+echo "[ Commits ](https://github.com/${PROJECT_REPO}/compare/${START_TAG}...${DEST_TAG})" >> ${FILE_CHANGELOG}
 echo "--------------------"
 echo "generate changelog to ${FILE_CHANGELOG}"

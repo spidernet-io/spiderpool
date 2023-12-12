912899ed061813fc40b1979765692d621b39d3d8
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2023-12-12 15:47:58 +0800

    ipoib
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- images/spiderpool-plugins/entrypoint.sh
+++ images/spiderpool-plugins/entrypoint.sh
@@ -7,11 +7,12 @@ set -e
 
 function usage()
 {
-    echo -e "--install-cni     enable install cni-plugins"
-    echo -e "--install-ovs     enable install ovs-plugin"
+    echo -e "--install-cni          enable install cni-plugins"
+    echo -e "--install-ovs          enable install ovs-plugin"
     echo -e "--install-ib-sriov     enable install ib-sriov"
-    echo -e "--install-rdma    enable install rdma-plugin"
-    echo -e "--copy-dst-dir    specifies the path to these plugins installed"
+    echo -e "--install-ipoib        enable install ipoib"
+    echo -e "--install-rdma         enable install rdma-plugin"
+    echo -e "--copy-dst-dir         specifies the path to these plugins installed"
 }
 
 COPY_DST_DIR="/host/opt/cni/bin"
@@ -19,10 +20,13 @@ CNI_SRC_DIR="/usr/src/cni/bin"
 OVS_SRC_DIR="/usr/src/cni/ovs-cni/ovs"
 RDMA_SRC_DIR="/usr/src/cni/rdma-cni/rdma"
 IB_SRIOV_SRC_DIR="/usr/src/cni/ib-sriov/ib-sriov"
+IPOIB_SRC_DIR="/usr/src/cni/ipoib/ipoib"
+
 INSTALL_CNI_PLUGINS=${INSTALL_CNI_PLUGINS:-false}
 INSTALL_OVS_PLUGIN=${INSTALL_OVS_PLUGIN:-false}
 INSTALL_RDMA_PLUGIN=${INSTALL_RDMA_PLUGIN:-false}
 INSTALL_IB_SRIOV_PLUGIN=${INSTALL_IB_SRIOV_PLUGIN:-false}
+INSTALL_IPOIB_PLUGIN=${INSTALL_IPOIB_PLUGIN:-false}
 
 # Parse parameters given as arguments to this script.
 while [ "$1" != "" ]; do
@@ -45,6 +49,9 @@ while [ "$1" != "" ]; do
         --install-ib-sriov)
             INSTALL_IB_SRIOV_PLUGIN=$VALUE
             ;;
+        --install-ipoib)
+            INSTALL_IPOIB_PLUGIN=$VALUE
+            ;;
         --copy-dst-dir)
             COPY_DST_DIR=$VALUE
             ;;
@@ -63,8 +70,7 @@ fi
 mkdir -p ${COPY_DST_DIR}
 
 if [ "$INSTALL_CNI_PLUGINS" = "true" ]; then
-    CNI_VERSION=$(cat VERSION.info | grep CNI_VERSION | awk '{print $2}')
-    echo "Installing CNI-Plugins: ${CNI_VERSION} ..."
+    echo "Installing CNI-Plugins: $(cat VERSION.info | grep CNI_VERSION | awk '{print $2}') ..."
     for plugin in "${CNI_SRC_DIR}"/*; do
       ITEM=${plugin##*/}
       rm -f ${COPY_DST_DIR}/${ITEM}.old || true
@@ -75,8 +81,7 @@ if [ "$INSTALL_CNI_PLUGINS" = "true" ]; then
 fi
 
 if [ "$INSTALL_OVS_PLUGIN" = "true" ]; then
-   OVS_VERSION=$(cat VERSION.info | grep OVS_VERSION | awk '{print $2}')
-   echo "Installing OVS-Plugin: ${OVS_VERSION} ..."
+   echo "Installing OVS-Plugin: $(cat VERSION.info | grep OVS_VERSION | awk '{print $2}') ..."
    rm -f ${COPY_DST_DIR}/ovs.old || true
    ( [ -f "${COPY_DST_DIR}/ovs" ] && mv ${COPY_DST_DIR}/ovs ${COPY_DST_DIR}/ovs.old ) || true
    cp ${OVS_SRC_DIR} ${COPY_DST_DIR}
@@ -84,8 +89,7 @@ if [ "$INSTALL_OVS_PLUGIN" = "true" ]; then
 fi
 
 if [ "$INSTALL_RDMA_PLUGIN" = "true" ]; then
-   RDMA_COMMIT_HASH=$(cat VERSION.info | grep RDMA_COMMIT_HASH | awk '{print $2}')
-   echo "Installing RDMA-Plugin: ${RDMA_COMMIT_HASH} ..."
+   echo "Installing RDMA-Plugin: $(cat VERSION.info | grep RDMA_COMMIT_HASH | awk '{print $2}') ..."
    rm -f ${COPY_DST_DIR}/rdma.old || true
    ( [ -f "${COPY_DST_DIR}/rdma" ] && mv ${COPY_DST_DIR}/rdma ${COPY_DST_DIR}/rdma.old ) || true
    cp ${RDMA_SRC_DIR} ${COPY_DST_DIR}
@@ -93,12 +97,19 @@ if [ "$INSTALL_RDMA_PLUGIN" = "true" ]; then
 fi
 
 if [ "$INSTALL_IB_SRIOV_PLUGIN" = "true" ]; then
-   RDMA_COMMIT_HASH=$(cat VERSION.info | grep IB_SRIOV_VERSION | awk '{print $2}')
-   echo "Installing ib-sriov: ${IB_SRIOV_VERSION} ..."
+   echo "Installing ib-sriov: $(cat VERSION.info | grep IB_SRIOV_VERSION | awk '{print $2}') ..."
    rm -f ${COPY_DST_DIR}/ib-sriov.old || true
    ( [ -f "${COPY_DST_DIR}/ib-sriov" ] && mv ${COPY_DST_DIR}/ib-sriov ${COPY_DST_DIR}/ib-sriov.old ) || true
-   cp ${RDMA_SRC_DIR} ${COPY_DST_DIR}
+   cp ${IB_SRIOV_SRC_DIR} ${COPY_DST_DIR}
    rm -f ${COPY_DST_DIR}/ib-sriov.old &>/dev/null  || true
+fi
+
+if [ "$INSTALL_IPOIB_PLUGIN" = "true" ]; then
+   echo "Installing ipoib: $(cat VERSION.info | grep IPOIB_VERSION | awk '{print $2}') ..."
+   rm -f ${COPY_DST_DIR}/ipoib.old || true
+   ( [ -f "${COPY_DST_DIR}/ipoib" ] && mv ${COPY_DST_DIR}/ipoib ${COPY_DST_DIR}/ipoib.old ) || true
+   cp ${IPOIB_SRC_DIR} ${COPY_DST_DIR}
+   rm -f ${COPY_DST_DIR}/ipoib.old &>/dev/null  || true
 fi
 
 echo Done.

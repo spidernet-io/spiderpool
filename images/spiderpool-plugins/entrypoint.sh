434d3d93eca1c04f31a707bcd57c4a23b5412e4f
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2023-12-14 12:13:58 +0800

    d
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- images/spiderpool-plugins/entrypoint.sh
+++ images/spiderpool-plugins/entrypoint.sh
@@ -16,17 +16,26 @@ function usage()
 }
 
 COPY_DST_DIR="/host/opt/cni/bin"
-CNI_SRC_DIR="/usr/src/cni/bin"
-OVS_SRC_DIR="/usr/src/cni/ovs-cni/ovs"
-RDMA_SRC_DIR="/usr/src/cni/rdma-cni/rdma"
-IB_SRIOV_SRC_DIR="/usr/src/cni/ib-sriov/ib-sriov"
-IPOIB_SRC_DIR="/usr/src/cni/ipoib/ipoib"
 
-INSTALL_CNI_PLUGINS=${INSTALL_CNI_PLUGINS:-false}
+echo "OVS_BIN_PATH=${OVS_BIN_PATH}"
+echo "IB_SRIOV_BIN_PATH=${IB_SRIOV_BIN_PATH}"
+echo "CNI_BIN_DIR=${CNI_BIN_DIR}"
+echo "IPOIB_BIN_PATH=${IPOIB_BIN_PATH}"
+echo "VERSION_FILE_PATH=${VERSION_FILE_PATH}"
+echo "RDMA_BIN_PATH=${RDMA_BIN_PATH}"
+
+[ -f "${RDMA_BIN_PATH}" ] || echo "error, failed to find ${RDMA_BIN_PATH}"
+[ -f "${OVS_BIN_PATH}" ] || echo "error, failed to find ${OVS_BIN_PATH}"
+[ -f "${IB_SRIOV_BIN_PATH}" ] || echo "error, failed to find ${IB_SRIOV_BIN_PATH}"
+[ -f "${IPOIB_BIN_PATH}" ] || echo "error, failed to find ${IPOIB_BIN_PATH}"
+[ -f "${VERSION_FILE_PATH}" ] || echo "error, failed to find ${VERSION_FILE_PATH}"
+[ -d "${CNI_BIN_DIR}" ] || echo "error, failed to find ${CNI_BIN_DIR}"
+
 INSTALL_OVS_PLUGIN=${INSTALL_OVS_PLUGIN:-false}
 INSTALL_RDMA_PLUGIN=${INSTALL_RDMA_PLUGIN:-false}
 INSTALL_IB_SRIOV_PLUGIN=${INSTALL_IB_SRIOV_PLUGIN:-false}
 INSTALL_IPOIB_PLUGIN=${INSTALL_IPOIB_PLUGIN:-false}
+INSTALL_CNI_PLUGINS=${INSTALL_CNI_PLUGINS:-false}
 
 # Parse parameters given as arguments to this script.
 while [ "$1" != "" ]; do
@@ -72,7 +81,7 @@ mkdir -p ${COPY_DST_DIR}
 if [ "$INSTALL_CNI_PLUGINS" = "true" ]; then
     VERSION=$(cat VERSION.info | grep CNI_VERSION | awk '{print $2}')
     echo "Installing CNI-Plugins: ${VERSION}"
-    for plugin in "${CNI_SRC_DIR}"/*; do
+    for plugin in "${CNI_BIN_DIR}"/*; do
       ITEM=${plugin##*/}
       rm -f ${COPY_DST_DIR}/${ITEM}.old || true
       ( [ -f "${COPY_DST_DIR}/${ITEM}" ] && mv ${COPY_DST_DIR}/${ITEM} ${COPY_DST_DIR}/${ITEM}.old ) || true
@@ -86,7 +95,7 @@ if [ "$INSTALL_OVS_PLUGIN" = "true" ]; then
    echo "Installing OVS-Plugin: ${VERSION}"
    rm -f ${COPY_DST_DIR}/ovs.old || true
    ( [ -f "${COPY_DST_DIR}/ovs" ] && mv ${COPY_DST_DIR}/ovs ${COPY_DST_DIR}/ovs.old ) || true
-   cp ${OVS_SRC_DIR} ${COPY_DST_DIR}
+   cp ${OVS_BIN_PATH} ${COPY_DST_DIR}
    rm -f ${COPY_DST_DIR}/ovs.old &>/dev/null  || true
 fi
 
@@ -95,7 +104,7 @@ if [ "$INSTALL_RDMA_PLUGIN" = "true" ]; then
    echo "Installing RDMA-Plugin: ${VERSION}"
    rm -f ${COPY_DST_DIR}/rdma.old || true
    ( [ -f "${COPY_DST_DIR}/rdma" ] && mv ${COPY_DST_DIR}/rdma ${COPY_DST_DIR}/rdma.old ) || true
-   cp ${RDMA_SRC_DIR} ${COPY_DST_DIR}
+   cp ${RDMA_BIN_PATH} ${COPY_DST_DIR}
    rm -f ${COPY_DST_DIR}/rdma.old &>/dev/null  || true
 fi
 
@@ -104,7 +113,7 @@ if [ "$INSTALL_IB_SRIOV_PLUGIN" = "true" ]; then
    echo "Installing ib-sriov: ${VERSION}"
    rm -f ${COPY_DST_DIR}/ib-sriov.old || true
    ( [ -f "${COPY_DST_DIR}/ib-sriov" ] && mv ${COPY_DST_DIR}/ib-sriov ${COPY_DST_DIR}/ib-sriov.old ) || true
-   cp ${IB_SRIOV_SRC_DIR} ${COPY_DST_DIR}
+   cp ${IB_SRIOV_BIN_PATH} ${COPY_DST_DIR}
    rm -f ${COPY_DST_DIR}/ib-sriov.old &>/dev/null  || true
 fi
 
@@ -113,10 +122,8 @@ if [ "$INSTALL_IPOIB_PLUGIN" = "true" ]; then
    echo "Installing ipoib: ${VERSION}"
    rm -f ${COPY_DST_DIR}/ipoib.old || true
    ( [ -f "${COPY_DST_DIR}/ipoib" ] && mv ${COPY_DST_DIR}/ipoib ${COPY_DST_DIR}/ipoib.old ) || true
-   cp ${IPOIB_SRC_DIR} ${COPY_DST_DIR}
+   cp ${IPOIB_BIN_PATH} ${COPY_DST_DIR}
    rm -f ${COPY_DST_DIR}/ipoib.old &>/dev/null  || true
 fi
 
 echo Done.
-
-ib-sriov

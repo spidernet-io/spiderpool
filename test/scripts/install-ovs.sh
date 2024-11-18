3b26931be47834977762e1faa8759af7f7b5a3ff
tao.yang <tao.yang@daocloud.io>
2024-11-18 10:35:03 +0800

    fix: after the installation of openvswitch failed, retrying did not work.
    Signed-off-by: tao.yang <tao.yang@daocloud.io>

--- test/scripts/install-ovs.sh
+++ test/scripts/install-ovs.sh
@@ -66,21 +66,24 @@ for NODE in $KIND_NODES; do
   install_openvswitch() {
     for attempt in {1..5}; do
       echo "Attempt $attempt to install openvswitch on ${NODE}..."
-      docker exec ${NODE} apt-get update > /dev/null
-      docker exec ${NODE} apt-get install -y apt-utils > /dev/null
-      docker exec ${NODE} apt-get install -y openvswitch-switch > /dev/null
-      
-      if [[ $? -eq 0 ]]; then
-        echo "Openvswitch installed successfully on ${NODE}"
-        return 0
+      if ! docker exec ${NODE} apt-get update > /dev/null; then
+        echo "Failed to update package list on ${NODE}, retrying in 10s..."
+        sleep 10
+        continue
       fi
-
-      echo "Failed to install openvswitch on ${NODE}, retrying in 10s..."
-      sleep 10
+      
+      if ! docker exec ${NODE} apt-get install -y openvswitch-switch > /dev/null; then
+        echo "Failed to install openvswitch on ${NODE}, retrying in 10s..."
+        sleep 10
+        continue
+      fi
+      
+      echo "Succeed to install openvswitch on ${NODE}"
+      return 0
     done
-    
-    echo "Error: Failed to install openvswitch on ${NODE} after 5 attempts"
-    exit 1
+
+    echo "Error: Failed to install openvswitch on ${NODE} after 5 attempts." >&2
+    return 1
   }
   
   echo "=========install openvswitch"

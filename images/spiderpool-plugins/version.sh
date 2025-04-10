451d7627c21589c7aeaebffc0cb97cc756c5db98
weizhoublue <45163302+weizhoublue@users.noreply.github.com>
2025-04-10 20:01:21 +0000

    robot updated CNI plugins version
    Signed-off-by: weizhoublue <weizhou.lan@daocloud.io>

--- images/spiderpool-plugins/version.sh
+++ images/spiderpool-plugins/version.sh
@@ -8,12 +8,12 @@
 # https://github.com/containernetworking/plugins
 export CNI_VERSION=${CNI_VERSION:-"v1.6.2"}
 # https://github.com/k8snetworkplumbingwg/ovs-cni
-export OVS_VERSION=${OVS_VERSION:-"v0.38.2"}
+export OVS_VERSION=${OVS_VERSION:-"v0.29.6"}
 # https://github.com/k8snetworkplumbingwg/rdma-cni
-export RDMA_VERSION=${RDMA_VERSION:-"v1.2.1"}
+export RDMA_VERSION=${RDMA_VERSION:-"v1.3.0"}
 # https://github.com/k8snetworkplumbingwg/sriov-cni
 export SRIOV_VERSION=${SRIOV_VERSION:-"v2.9.0"}
 # https://github.com/k8snetworkplumbingwg/ib-sriov-cni
-export IB_SRIOV_VERSION=${IB_SRIOV_VERSION:-"v1.2.0"}
+export IB_SRIOV_VERSION=${IB_SRIOV_VERSION:-"v1.2.1"}
 # https://github.com/Mellanox/ipoib-cni
-export IPOIB_VERSION=${IPOIB_VERSION:-"v1.2.1"}
+export IPOIB_VERSION=${IPOIB_VERSION:-"v1.2.2"}

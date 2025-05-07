4516eaac0c2256b6232e78004fc3d2aa538af622
Cyclinder Kuo <kuocyclinder@gmail.com>
2025-05-07 14:46:22 +0800

    Get allocated nvidia gpus from pod namespace
    Signed-off-by: Cyclinder Kuo <kuocyclinder@gmail.com>

--- pkg/dra/utils.go
+++ pkg/dra/utils.go
@@ -3,8 +3,10 @@ package dra
 import (
 	"net/url"
 	"os"
+	"regexp"
 	"strings"
 	"time"
+	"unicode"
 
 	"github.com/spidernet-io/spiderpool/pkg/constant"
 )
@@ -44,4 +46,28 @@ func localEndpoint(path string) *url.URL {
 		Scheme: unixProtocol,
 		Path:   path + ".sock",
 	}
+}
+
+// NormalizedDNS1123Label normalizes the interface name to a valid DNS1123 label
+func NormalizedDNS1123Label(iface string) string {
+	// Convert to lowercase
+	normalized := strings.ToLower(iface)
+	// Replace invalid chars with hyphen
+	reg := regexp.MustCompile("[^a-z0-9-]")
+	normalized = reg.ReplaceAllString(normalized, "-")
+	// Remove leading and trailing hyphens
+	normalized = strings.Trim(normalized, "-")
+	// Replace multiple consecutive hyphens with a single one
+	reg = regexp.MustCompile("-+")
+	normalized = reg.ReplaceAllString(normalized, "-")
+
+	// If the string is empty after normalization, use a default name
+	if normalized == "" {
+		normalized = "iface"
+	}
+	// If it starts with a number, prefix it
+	if unicode.IsDigit(rune(normalized[0])) {
+		normalized = "iface-" + normalized
+	}
+	return normalized
 }
